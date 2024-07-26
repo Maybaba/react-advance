357d8c42075d96618c0fdfff0618569ef8dff838
Maybaba <sinyunjong@gmail.com>
2024-07-26 09:18:02 +0900

    # redux study set

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React, { useState } from 'react';
+import ReduxCounter from './redux-practice/components/ReduxCounter';
 
-import Counter from './optimizing/components/Counter/Counter';
-import Header from './optimizing/components/Header';
-import { log } from './optimizing/log';
-import ConfigureCounter from './optimizing/components/Counter/ConfigureCounter';
+
 
 const App = () => {
-  log('<App /> rendered');
 
-  const [chosenCount, setChosenCount] = useState(0);
 
-  const setCountHandler = number => {
-    setChosenCount(number);
-  }
+  
 
   return (
     <>
-      <Header />
-      <main>
-        <ConfigureCounter onSet={setCountHandler} />
-        <Counter key={chosenCount} initialCount={chosenCount} />
-      </main>
+    <ReduxCounter />
     </>
   );
 };
