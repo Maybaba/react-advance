d6001d78f541438abcf4c2206d0b4cdca7c89e56
Maybaba <sinyunjong@gmail.com>
2024-07-26 10:59:04 +0900

    # multi slice

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React, { useState } from 'react';
 import ReduxCounter from './redux-practice/components/ReduxCounter';
-
-
+import Header from './redux-practice/components/Header';
+import Auth from './redux-practice/components/Auth';
 
 const App = () => {
 
-
-  
-
   return (
     <>
-    <ReduxCounter />
+      <Header />
+      <Auth />
+      <ReduxCounter />
     </>
   );
 };
