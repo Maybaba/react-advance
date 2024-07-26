0087c36f494835c50276b8d771fcc653b38148a3
Maybaba <sinyunjong@gmail.com>
2024-07-26 10:51:50 +0900

    # redux toolkit basic

--- src/redux-practice/components/ReduxCounter.js
+++ src/redux-practice/components/ReduxCounter.js
@@ -1,51 +1,53 @@
 import React from 'react';
 import styles from './ReduxCounter.module.css';
 import { useDispatch, useSelector } from 'react-redux';
-import { INCREMENT, DECREMENT, MULTIPLY, TOGGLE } from '../store';
+import { counterActions } from '../store';
 
 const ReduxCounter = () => {
-  //useSelector 라는 훅을 통해 redux store에 이쓴 상태값을 가져옴
+
+  // useSelector라는 훅을 통해 redux store에 있는 상태값을 가져옴
   const counter = useSelector(state => state.counter);
   const show = useSelector(state => state.showCounter);
 
-  // 리덕스 스토어에 상태값 변경을 위해 액션을 호출하는 훅 like setter
+  // 리덕스 스토어에 상태값 변경을 위해 액션을 호출하는 훅
   const dispatch = useDispatch();
 
-  // state 증가 함수
-  const increseHandler = e => {
-    // redux store에 접근해서 상태값을 변경시켜야 함
-    // 리덕스에서는 상태값 변경을 위해 액션 함수를 호출해야 함
-    // 액션 함수는 리덕스 스토어 내부에 있는 리듀서가 가지고 있음
+  const { increment, decrement, multiply, toggle } = counterActions;
 
-    //dispatch parameter로 어떤 변경을 할 지 type과 변경이 필요한 payload를 전송
-    dispatch({type: INCREMENT});
+  const increaseHandler = e => {
+    // redux store에 접근해서 상태값을 변경시켜야 함
+    // 리덕스에서는 상태값 변경을 위해 액션함수를 호출해야 함.
+    // 액션 함수는 리덕스 스토어 내부에 있는 리듀서가 가지고 있음.
+    
+    // dispatch에는 인자로 어떤 변경을 할지 type과 변경에 필요한
+    // payload를 전송
+    dispatch(increment());
   };
 
-  // state 감소 함수
-  const decrementHandler = e => {
-    dispatch({ type: DECREMENT});
+  const decreaseHandler = e => {
+    dispatch(decrement());
   };
 
   const multiplyHandler = e => {
-    dispatch({ type: MULTIPLY, payload: 4 }); //payload에 담아서 값을 전달할 수 있다.
-    // 그러면 reducer의 action에 들어온다.
+    dispatch(multiply(2));
   };
 
   const toggleHandler = e => {
-    dispatch({ type: TOGGLE})
-  }
+    dispatch(toggle());
+  };
 
   return (
     <main className={styles.counter}>
       <h1>Redux Counter</h1>
-      { show && <div className={styles.value}>{counter}</div>}
 
+      {show && <div className={styles.value}>{counter}</div>}
+      
       <div>
-        <button onClick={increseHandler}>Increment</button>
-        <button onClick={decrementHandler}>Decrement</button>
-        <button onClick={multiplyHandler}>Double</button>
+        <button onClick={increaseHandler}>Increment</button>
+        <button onClick={decreaseHandler}>Decrement</button>
+        <button onClick={multiplyHandler}>IncrementDouble</button>
       </div>
-
+      
       <button onClick={toggleHandler}>Toggle Counter</button>
     </main>
   );
--- src/redux-practice/store/index.js
+++ src/redux-practice/store/index.js
@@ -1,15 +1,11 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-import { createStore } from "redux";
+
 // action type constant : 보통 다른파일에 상수만 모아두고 관리한다. 
-
-export const INCREMENT = 'INCREMENT';
-export const DECREMENT = 'DECREMENT';
-export const MULTIPLY = 'MULTIPLY';
-export const TOGGLE = 'TOGGLE';
 
 // 관리할 초기 상태값 객체
 const initialCountState = {
-  counter: 0,
+  counter: 0, //name
   showCounter: true
 };
 
@@ -20,44 +16,45 @@ param1 (state): 상태변경 이전의 상태
 param2 (action): 상태를 어떻게 변경할지에 대한 명세
 return - 변경 후 새로운 상태값
 */
-const counterReducer = (state = initialCountState, action) => {
 
-  console.log('prev state: ', state);
-  console.log('action: ', action);
-
-  // 상태값 변경 시 새로운 상태를 return 하여야 함
-  // 1. 상태값 변경시 반드시 새로운 상태를 반환
-  // 2. 상태값 변경은 반드시 새로운 객체를 할당해야 함.
-  switch(action.type) {
-    case INCREMENT:
-      return {
-        ...state,
-        counter: state.counter + 1
-      };
-    case DECREMENT:
-      return {
-        ...state,
-        counter: state.counter - 1
-      };
-      case MULTIPLY:
-      return {
-        ...state,
-        counter: state.counter * action.payload
-      };
-      case TOGGLE:
-        return {
-          ...state,
-          showCounter: !(state.showCounter)
-        };
-
-    default:
-      return state;
+// reducer를 slice로 변경
+/*
+option 객체에 들어가 있는 프로퍼티 설명
+prop1 : name - 컴포넌트가 해당 리듀서를 사용할 때 부르는 이름
+prop2 : initialState - 관리할 상태값들의 초기값
+prop3 : reducers - 기존 리듀서에서 사용하던 내용들(실제 액션)
+*/
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState: initialCountState,
+  reducers: {
+    // 자동으로 복사를 해준다.
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    multiply(state, action) {
+      console.log("state :",state);
+      console.log("action :",action);
+      state.counter *= action.payload;
+      // immer 라이브러리를 활용하여 자동으로 깊은복사를 해주기 때문에 복사를 생각하지 않고
+      // 더 편하게 상요할 수 있다. 
+    },
+    toggle(state) {
+      state.showCounter = !(state.showCounter);
+    },
   }
-};
-
+});
 
 // 단 하나의 리덕스 스토어 :  createStore 먼저 이해 후 configurestore 사용
-const store = createStore(counterReducer);
+const store = configureStore({
+  reducer: counterSlice.reducer
+});
+
+// 슬라이스 안에 reduvers에 정의한 함수들을 내보내기
+export const counterActions = counterSlice.actions;
 
 //리액트의 index.js에게 store를 제공
 export default store;
