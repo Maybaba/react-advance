6227b2ca12f41bb7bebda2690aa2a0ee8f851146
Maybaba <sinyunjong@gmail.com>
2024-07-26 09:57:43 +0900

    # redux action reducer store BASIC

--- src/redux-practice/components/ReduxCounter.js
+++ src/redux-practice/components/ReduxCounter.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './ReduxCounter.module.css';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { INCREMENT, DECREMENT } from '../store';
 
 const ReduxCounter = () => {
 
@@ -8,14 +9,32 @@ const ReduxCounter = () => {
   //useSelector 라는 훅을 통해 redux store에 이쓴 상태값을 가져옴
   const counter = useSelector(state => state.counter);
 
+  // 리덕스 스토어에 상태값 변경을 위해 액션을 호출하는 훅 like setter
+  const dispatch = useDispatch();
+
+  // state 증가 함수
+  const increseHandler = e => {
+    // redux store에 접근해서 상태값을 변경시켜야 함
+    // 리덕스에서는 상태값 변경을 위해 액션 함수를 호출해야 함
+    // 액션 함수는 리덕스 스토어 내부에 있는 리듀서가 가지고 있음
+
+    //dispatch parameter로 어떤 변경을 할 지 type과 변경이 필요한 payload를 전송
+    dispatch({type: INCREMENT});
+  };
+
+  // state 감소 함수
+  const decrementHandler = e => {
+    dispatch({ type: DECREMENT});
+  };
+
   return (
     <main className={styles.counter}>
       <h1>Redux Counter</h1>
       <div className={styles.value}>{counter}</div>
 
       <div>
-        <button>Increment</button>
-        <button>Decrement</button>
+        <button onClick={increseHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
       </div>
 
       <button>Toggle Counter</button>
--- src/redux-practice/store/index.js
+++ src/redux-practice/store/index.js
@@ -1,11 +1,14 @@
 
 import { createStore } from "redux";
 
+// action type constant : 보통 다른파일에 상수만 모아두고 관리한다. 
+export const INCREMENT = 'INCREMENT';
+export const DECREMENT = 'DECREMENT';
+
 // 관리할 초기 상태값 객체
 const initialCountState = {
-  counter: 10,
+  counter: 0,
 };
-
 
 //reducer: 상태 변경을 위한 순수 함수 (부수효과 (비동기코드) 가 없는 함수)
 //카운터 상태 관리를 위한 reducer 정의
@@ -19,13 +22,23 @@ const counterReducer = (state = initialCountState, action) => {
   console.log('prev state: ', state);
   console.log('action: ', action);
 
-  return state;
-
-  // switch() {
-  //   case 
-  // }
-
+  // 상태값 변경 시 새로운 상태를 return 하여야 함
+  // 1. 상태값 변경시 반드시 새로운 상태를 반환
+  // 2. 상태값 변경은 반드시 새로운 객체를 할당해야 함.
+  switch(action.type) {
+    case INCREMENT:
+      return {
+        counter: state.counter + 1
+      };
+    case DECREMENT:
+      return {
+        counter: state.counter - 1
+      };
+    default:
+      return state;
+  }
 };
+
 
 // 단 하나의 리덕스 스토어 :  createStore 먼저 이해 후 configurestore 사용
 const store = createStore(counterReducer);
